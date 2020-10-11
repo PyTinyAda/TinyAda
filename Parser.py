fe913d1eba6b97dc19c1454bbcab9122b284e502
ByeongHeon Kang <kbh122369@gmail.com>
2020-10-11 21:08:06 +0900

    fixed miner error

--- Parser.py
+++ Parser.py
@@ -165,7 +165,7 @@ class Parser:
         if self.token.code == Token.IN:
             self.token = self.scanner.nextToken()
             if self.token.code == Token.OUT:
-                self.token = self.scanner.nextTOken()
+                self.token = self.scanner.nextToken()
         elif self.token.code == Token.OUT:
             self.token = self.scanner.nextToken()
 
